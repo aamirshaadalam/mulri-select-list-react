1f5da234c16af9ec7fdd14e81453368c5b828a01
aamirshaadalam <aamirshaad.alam@gmail.com>
2021-09-21 23:58:49 +0530

    total pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
 import './css/App.css';
 import List from './lib/list';
 import data from './data/cities.json';
-
-const getData = (config) => {
-  const { pageNumber, searchText, pageSize } = config;
-  let result = data;
-
-  console.log(`Total number of records: ${data.length}`);
-
-  if (searchText) {
-    result = data.filter((item) => {
-      return item.caption.toLowerCase().includes(searchText);
-    });
-  }
-
-  if (pageNumber > 0 && pageSize) {
-    const start = (pageNumber - 1) * pageSize;
-    const end = start + pageSize;
-
-    result = result.slice(start, end);
-  }
-
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(result);
-    }, 1000);
-  });
-};
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  const [totalPages, setTotalPages] = useState(0);
+
+  const computePageCount = (result, pageSize) => {
+    let pages = result.length / pageSize;
+
+    if (result.length % pageSize === 0) {
+      setTotalPages(pages);
+    } else {
+      setTotalPages(pages + 1);
+    }
+  };
+
+  const getData = (config) => {
+    const { pageNumber, searchText, pageSize } = config;
+    let result = data;
+
+    console.log(`Total number of records: ${data.length}`);
+
+    if (searchText) {
+      result = data.filter((item) => {
+        return item.caption.toLowerCase().includes(searchText);
+      });
+    }
+
+    computePageCount(result, pageSize);
+
+    if (pageNumber > 0 && pageSize) {
+      const start = (pageNumber - 1) * pageSize;
+      const end = start + pageSize;
+
+      result = result.slice(start, end);
+    }
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(result);
+      }, 1000);
+    });
+  };
+
   return (
     <List
       loadCallback={getData}
@@ -36,7 +51,8 @@ function App() {
       singleSelect
       noRecordsMessage='No Records Found'
       pageSize={25}
-      totalPages={40}></List>
+      totalPages={totalPages}
+      searchAtServer></List>
   );
 }
 
